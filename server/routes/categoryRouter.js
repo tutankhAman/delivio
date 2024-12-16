b209a35e4391180ab30e3ce50b9eb5d1b43ed73c
dxrken <132681496+tutankhAman@users.noreply.github.com>
2024-12-16 12:24:04 +0530

    CRUD operations implemented for categories

--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -7,4 +7,8 @@ router.route('/category')
 .get(categoryCtrl.getCategories)
 .post(auth, authAdmin, categoryCtrl.createCategory)
 
+router.route('/category/:id')
+.delete(auth, authAdmin, categoryCtrl.deleteCategory)
+.put(auth, authAdmin, categoryCtrl.updateCategory)
+
 module.exports = router
